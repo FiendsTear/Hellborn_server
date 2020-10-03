10fff250db978f240492d9f57e19f6eb9bb031ef
FiendsTear <94nerevar@gmail.com>
2020-10-03 12:23:12 +0400

    initial commit

---
+++ build/Index.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var players = {};
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on("new-player", function (data) {
+        socket.emit("update-players", players);
+        socket.broadcast.emit("create-player", { id: socket.id });
+        players[socket.id] = { x: 700, y: 1000 };
+    });
+    socket.on("new-place", function (data) {
+        socket.broadcast.emit("new-place", { id: socket.id, x: data.x, y: data.y });
+    });
+});
+http.listen(4000, function () {
+    console.log('listening on *:4000');
+});
+//# sourceMappingURL=Index.js.map
